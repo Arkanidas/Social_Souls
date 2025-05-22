2e113828cb02661b955e5d2d2bbed0d37a18e8a9
Leon <xleonidasprox@gmail.com>
2025-05-22 02:19:58 +0200

    text and size adjustments

--- socialsouls/src/Landing.tsx
+++ socialsouls/src/Landing.tsx
@@ -27,41 +27,66 @@ function Landing() {
 
         <div className="w-[200%] h-full relative flex transition-transform duration-500 ease-out peer-checked:-translate-x-1/2">
           {/* Sign In */}
-          <form className="w-1/2 flex flex-col justify-center items-center gap-12 p-5">
+          <form className="w-1/2 flex flex-col justify-center items-center gap-12">
             <h2 className="text-4xl font-bold text-[#ffeedd] font-[spook1]">Sign In</h2>
 
             
 
-            <div className="relative w-[250px]">
+  <div className="relative w-[250px]">
   <input
     type="text"
     id="username"
     required
-    className="peer w-full text-white bg-transparent border-0 border-b-2 border-gray-400 placeholder-transparent focus:border-[#f34040] outline-none text-[17px]"
-   
+    className="peer w-full text-white bg-transparent border-0 border-b-2 border-gray-400 placeholder-transparent focus:border-[#FF8C19] outline-none text-[17px]  "
+   placeholder="Username"
   />
   <label
     htmlFor="username"
-    className="absolute left-0 bottom-1 text-[17px] text-gray-400 font-[ifont] transition-all duration-300 transform peer-placeholder-shown:translate-y-6 peer-placeholder-shown:text-[17px] peer-focus:translate-y-[-10px] peer-focus:text-[20px] peer-focus:text-[#f34040]"
-  >
+    className="
+    absolute left-0 bottom-1 text-[17px] font-[ifont] text-gray-400
+    transition-all duration-200 transform
+    peer-placeholder-shown:text-[17px]
+    peer-focus:translate-y-[-20px]
+    peer-focus:text-[22px]
+    peer-focus:text-[#FF8C19]
+    peer-valid:translate-y-[-20px]
+    peer-valid:text-[22px]
+    peer-valid:text-gray-400
+    cursor-text">
+
     Username
   </label>
-  
-  <FontAwesomeIcon icon={faUser} className="absolute right-0 top-2 -translate-y-1/2 text-gray-400" />
+  <FontAwesomeIcon icon={faUser} className="absolute right-0 top-2 -translate-y-1/2 text-gray-400 text-lg" />
 </div>
 
 
 
-            <div className="relative w-[250px]">
-              <input
-                type="password"
-                required
-                className="peer w-full text-white bg-transparent border-0 border-b-2 border-gray-400 placeholder-transparent focus:border-[#f34040] outline-none text-[17px]"
-              />
-              <label className="absolute left-0 bottom-1 text-[17px] text-gray-400 font-[ifont] transition-all duration-300 transform peer-placeholder-shown:translate-y-6 peer-placeholder-shown:text-[17px] peer-focus:translate-y-[-10px] peer-focus:text-[20px] peer-focus:text-[#f34040]">
+<div className="relative w-[250px]">
+  <input
+  type="password"
+  id="password"
+  required
+  placeholder="Password"
+  className="peer w-full text-white bg-transparent border-0 border-b-2 border-gray-400 placeholder-transparent focus:border-[#FF8C19] outline-none text-[17px]"
+   />
+   <label 
+ className="
+ absolute left-0 bottom-1 text-[17px] font-[ifont] text-gray-400
+ transition-all duration-200 transform
+ peer-placeholder-shown:text-[17px]
+ peer-focus:translate-y-[-20px]
+ peer-focus:text-[22px]
+ peer-focus:text-[#FF8C19]
+ peer-valid:translate-y-[-20px]
+ peer-valid:text-[22px]
+ peer-valid:text-gray-400
+ cursor-text"     
+ htmlFor="password"
+   >
+ 
                 Password
               </label>
-              <FontAwesomeIcon icon={faLock} className="absolute right-0 top-1/2 -translate-y-1/2 text-gray-400" />
+              <FontAwesomeIcon icon={faLock} className="absolute right-0 top-2 -translate-y-1/2 text-gray-400 text-lg"/>
             </div>
 
             <button className="w-[250px] h-[45px] bg-gradient-to-r from-[#e5e5e5] via-[#ff8c19] to-[#f34040] bg-[length:250%] bg-left rounded-[10px] relative text-[#ffd277] font-bold text-[16px] flex justify-center items-center transition-all duration-1000 hover:bg-right active:scale-95 overflow-hidden">
@@ -70,7 +95,7 @@ function Landing() {
               </span>
             </button>
 
-            <span className="text-sm text-[#d5d4d4] font-[sawarabi]">
+            <span className="text-sm text-[#d5d4d4] font-[sawarabi] mt-[-25px]">
               Don't have an account?{" "}
               <label htmlFor="register_toggle" className="underline font-bold cursor-pointer">
                 Sign Up
@@ -78,20 +103,36 @@ function Landing() {
             </span>
           </form>
 
+
+
+
+
           {/* Sign Up */}
-          <form className="w-1/2 flex flex-col justify-center items-center gap-6 p-6">
-            <h2 className="text-3xl font-bold text-[#ffeedd] font-[spook1]">Sign Up</h2>
+          <form className="w-1/2 flex flex-col justify-center items-center gap-10 p-6">
+            <h2 className="text-4xl font-bold text-[#ffeedd] font-[spook1]">Sign Up</h2>
 
             <div className="relative w-[250px]">
               <input
-                type="text"
+                type="email"
                 required
                 className="peer w-full text-white bg-transparent border-0 border-b-2 border-gray-400 placeholder-transparent focus:border-[#f34040] outline-none text-[17px]"
               />
-              <label className="absolute left-0 bottom-1 text-[17px] text-gray-400 font-[ifont] transition-all duration-300 transform peer-placeholder-shown:translate-y-6 peer-placeholder-shown:text-[17px] peer-focus:translate-y-[-10px] peer-focus:text-[20px] peer-focus:text-[#f34040]">
+              <label
+              htmlFor="Email"
+    className="
+    absolute left-0 bottom-1 text-[17px] font-[ifont] text-gray-400
+    transition-all duration-200 transform
+    peer-placeholder-shown:text-[17px]
+    peer-focus:translate-y-[-20px]
+    peer-focus:text-[22px]
+    peer-focus:text-[#FF8C19]
+    peer-valid:translate-y-[-20px]
+    peer-valid:text-[22px]
+    peer-valid:text-gray-400
+    cursor-text">
                 Email
               </label>
-              <FontAwesomeIcon icon={faEnvelope} className="absolute right-0 top-1/2 -translate-y-1/2 text-gray-400" />
+              <FontAwesomeIcon icon={faEnvelope} className="absolute right-0 top-2 -translate-y-1/2 text-gray-400 text-lg" />
             </div>
 
             <div className="relative w-[250px]">
@@ -101,10 +142,21 @@ function Landing() {
                 className="w-full border-0 border-b-2 border-gray-400 text-white bg-transparent placeholder-transparent focus:border-[#f34040] focus:font-bold outline-none text-[17px]"
                 placeholder="Username"
               />
-              <label className="absolute left-0 top-0 text-[17px] text-gray-400 font-[ifont] transition-all">
+              <label htmlFor="Username"
+    className="
+    absolute left-0 bottom-1 text-[17px] font-[ifont] text-gray-400
+    transition-all duration-200 transform
+    peer-placeholder-shown:text-[17px]
+    peer-focus:translate-y-[-20px]
+    peer-focus:text-[22px]
+    peer-focus:text-[#FF8C19]
+    peer-valid:translate-y-[-20px]
+    peer-valid:text-[22px]
+    peer-valid:text-gray-400
+    cursor-text">
                 Username
               </label>
-              <FontAwesomeIcon icon={faUser} className="absolute right-0 top-2 -translate-y-1/2 text-gray-400" />
+              <FontAwesomeIcon icon={faUser} className="absolute right-0 top-2 -translate-y-1/2 text-gray-400 text-lg" />
             </div>
 
             <div className="relative w-[250px]">
@@ -117,7 +169,7 @@ function Landing() {
               <label className="absolute left-0 top-0 text-[17px] text-gray-400 font-[ifont] transition-all">
                 Password
               </label>
-              <FontAwesomeIcon icon={faLock} className="absolute right-0 top-1/2 -translate-y-1/2 text-gray-400" />
+              <FontAwesomeIcon icon={faLock} className="absolute right-0 top-2 -translate-y-1/2 text-gray-400 text-lg" />
             </div>
 
             <button className="w-[250px] h-[45px] bg-gradient-to-r from-[#e5e5e5] via-[#ff8c19] to-[#f34040] bg-[length:250%] bg-left rounded-[10px] relative text-[#ffd277] font-bold text-[16px] flex justify-center items-center transition-all duration-1000 hover:bg-right active:scale-95 overflow-hidden">
